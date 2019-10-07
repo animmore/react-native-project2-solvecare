b13a2890edef2034fb7bacea6b233ee5859b3c7d
animmore <zaskokazhenya@gmail.com>
2019-10-07 15:44:33 +0300

    1st comp

---
+++ FormErrors.js
@@ -0,0 +1,17 @@
+// import React, {Component} from 'react'
+// import {StyleSheet, View, Text, TextInput, Button} from 'react-native'
+
+// export const FormErrors = ({formErrors}) =>
+//   <View className='formErrors'>
+//     {Object.keys(formErrors).map((fieldName, i) => {
+//       if(formErrors[fieldName].length > 0){
+//         return (
+//           <Text key={i}>{fieldName} {formErrors[fieldName]}</Text>
+//         )        
+//       } else {
+//         return '';
+//       }
+//     })}
+//   </View>
+
+  
